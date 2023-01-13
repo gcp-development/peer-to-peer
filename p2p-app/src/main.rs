ae412ef96aeab632ea8c8870270cef511991f918
gcp-development <bruno.silveira.backup@gmail.com>
2023-01-13 08:53:05 +0000

    p2p

--- p2p-app/src/main.rs
+++ p2p-app/src/main.rs
@@ -2,6 +2,8 @@ use futures::prelude::*;
 use libp2p::swarm::{Swarm, SwarmEvent};
 use libp2p::{identity, Multiaddr, PeerId, ping};
 use std::error::Error;
+use chrono::{Datelike, Timelike, Utc};
+use std::{thread, time};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -23,12 +25,43 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //Container port 4242
     swarm.listen_on("/ip4/0.0.0.0/tcp/4242".parse()?)?;
 
+    let default: String = String::from("default");
     // Dial the peer identified by the multi-address given as the second
     // command-line argument, if any.
     if let Some(addr) = std::env::args().nth(1) {
-        let remote: Multiaddr = addr.parse()?;
-        swarm.dial(remote)?;
-        println!("Dialed {}", addr)
+        if addr.eq(&default)
+        {
+            let delay = time::Duration::from_secs(60);
+            // Infinite loop
+            loop {
+                let now = Utc::now();
+                let (is_pm, hour) = now.hour12();
+                println!(
+                    "The current UTC time is {:02}:{:02}:{:02} {}",
+                    hour,
+                    now.minute(),
+                    now.second(),
+                    if is_pm { "PM" } else { "AM" }
+                );
+                let (is_common_era, year) = now.year_ce();
+                println!(
+                    "The current UTC date is {}-{:02}-{:02} {:?} ({})",
+                    year,
+                    now.month(),
+                    now.day(),
+                    now.weekday(),
+                    if is_common_era { "CE" } else { "BCE" }
+                );
+                println!("Waiting for cycle....");
+                thread::sleep(delay);
+            }
+        }
+        else
+        {
+            let remote: Multiaddr = addr.parse()?;
+            swarm.dial(remote)?;
+            println!("Dialed {}", addr)
+        }
     }
 
     loop {
