e96f29031de33584563a38d4568273f9e2dd1207
gcp-development <bruno.silveira.backup@gmail.com>
2023-04-20 10:03:23 +0100

    mdns-app

--- mdns-app/src/main.rs
+++ mdns-app/src/main.rs
@@ -1,10 +1,11 @@
 use futures::StreamExt;
 use libp2p::{
     identity, mdns,
-    swarm::{Swarm, SwarmEvent},
+    swarm::{SwarmEvent},
     PeerId,
 };
 use std::error::Error;
+use libp2p::swarm::SwarmBuilder;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -12,8 +13,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let peer_id = PeerId::from(id_keys.public());
     println!("Local peer id: {peer_id:?}");
     let transport = libp2p::development_transport(id_keys).await?;
-    let behaviour = mdns::async_io::Behaviour::new(mdns::Config::default())?;
-    let mut swarm = Swarm::with_async_std_executor(transport, behaviour, peer_id);
+    let behaviour = mdns::async_io::Behaviour::new(mdns::Config::default(),peer_id)?;
+
+    let mut swarm = SwarmBuilder::with_async_std_executor(transport, behaviour, peer_id).build();
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     loop {
